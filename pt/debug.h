1e2870f6464ea4f874ff8473d4f45bb63c1765b3
kirasys <kirasys0@naver.com>
2020-11-18 00:00:25 -0500

    Disable logging

--- pt/debug.h
+++ pt/debug.h
@@ -31,7 +31,6 @@
 /* _PRINTF is the standard logging enabled with -D */
 /* _DEBUG is activated with -d kafl cmdline */
 /* _ERROR is printed to stdout (or logged if logging is enabled) */
-#define DEBUG_QEMU
 #ifdef DEBUG_QEMU
 #define QEMU_PT_PRINTF(PREFIX, format, ...) qemu_log(QEMU_PT_PREFIX PREFIX format "\n", ##__VA_ARGS__)
 #define QEMU_PT_ERROR(PREFIX, format, ...)  printf(QEMU_PT_PREFIX PREFIX format "\n", ##__VA_ARGS__)
@@ -41,7 +40,7 @@
 #endif
 
 
-//#define PT_DEBUG_DISABLE
+#define PT_DEBUG_DISABLE
 #ifndef PT_DEBUG_DISABLE
 #define QEMU_PT_DEBUG(PREFIX, format, ...)  qemu_log_mask(LOG_KAFL, QEMU_PT_PREFIX PREFIX format "\n", ##__VA_ARGS__)
 #else
